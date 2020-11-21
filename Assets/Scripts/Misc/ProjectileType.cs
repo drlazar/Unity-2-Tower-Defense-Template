7c6ef9f7d142f41064cf5b6640a3ec6a32209fce
drlazar <drlazar@users.noreply.github.com>
2020-11-21 14:55:22 -0800

    Added Scriptable Projectiles and Animation

---
+++ Assets/Scripts/Misc/ProjectileType.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Projectile", menuName = "CustomObjects/Projectile")]
+public class ProjectileType : ScriptableObject
+{
+    public int damage;
+    public float size;
+    public float speed;
+    public Color color = Color.black;
+    public Sprite sprite;
+}
